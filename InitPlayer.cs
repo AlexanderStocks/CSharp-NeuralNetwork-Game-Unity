efd954d6e91749e0ec228f108e12f7587e281163
Alex Stocks <stocksalex@icloud.com>
2020-06-23 16:16:23 +0100

    Added input options for gravity changing, health and round ui's

--- InitPlayer.cs
+++ InitPlayer.cs
@@ -1,45 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InitPlayer : MonoBehaviour
 {
-    private const float jumpVelocity = 10;
-    private const float xSpeed = 6f;
-    private const float gravity = 5;
 
-    Vector3 velocity;
+    private const float JUMP_VELOCITY = 10f;
+    private const float PLAYER_SPEED = 6f;
+    private const float GRAVITY_STRENGTH = 5f;
+        
+    private float gravity;
 
-    Player player;
+    private Vector3 velocity;
 
+    private Player player;
+
+    private GameObject roundUI;
+    private GameObject healthUI;
+
+    private const int START_HEALTH = 100;
+    private int health;
+
+    private int roundNumber;
+
+    private bool isDead;
+
+    private GravityDirection gravityDirection;
+
+    PlayerControl playerControl;//outputscript
     // Start is called before the first frame update
     void Start()
     {
+        health = START_HEALTH;
         player = GetComponent<Player>();
+        isDead = false;
+
+        gravityDirection = GravityDirection.DOWN;
+        gravity = GRAVITY_STRENGTH * -1;
+
+        playerControl = GetComponent<PlayerControl>();
+
+        health = START_HEALTH;
+        isDead = false;
+
+        roundNumber = 0;
+    }
+
+    public void KillPlayer()
+    {
+        Destroy(gameObject);//destory the player game object
+        SceneManager.LoadScene("Death");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+        {
+            KillPlayer();
+        }
+
         if (player.sideOfCollision.getDown())
         {
             velocity.y = Mathf.Clamp(velocity.y, 0, float.MaxValue);
         }
 
+        if (health <= 0)
+        {
+            health = 0;
+            isDead = true;
+        }
+
+        //healthUI.gameObject.GetComponent<Text>().text = "Health: " + health;
+        //roundUI.gameObject.GetComponent<Text>().text = "Round: " + roundNumber;
+
+
         Vector2 userInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (player.sideOfCollision.getDown() && Input.GetKeyDown(KeyCode.Space))
         {
-            velocity.y = jumpVelocity;
+            velocity.y = JUMP_VELOCITY;
+        } else if (playerControl.sideOfCollision.getDown())
+        {
+            velocity.y = Mathf.Clamp(velocity.y, 0, float.MaxValue);
         }
 
-        velocity.x = userInput.x * xSpeed;
+        velocity.x = userInput.x * PLAYER_SPEED;
         velocity.y += gravity * Time.deltaTime;
 
-        player.UpdateFromInitPlayer(velocity * Time.deltaTime);
+        player.UpdateFromPlayer(velocity * Time.deltaTime);
     }
 
     public float getGravity() {
         return gravity;
     }
+
+    public void ChangeGravity()
+    {
+        gravityDirection = (GravityDirection)Random.Range(0, 4);
+
+        if (gravityDirection == GravityDirection.DOWN || gravityDirection == GravityDirection.LEFT)
+        {
+            gravity = (gravity > 0) ? gravity * -1 : gravity;
+        }
+        
+        if (gravityDirection == GravityDirection.UP || gravityDirection == GravityDirection.RIGHT)
+        {
+            gravity = (gravity < 0) ? gravity * -1 : gravity;
+        }
+
+        Invoke("ResetGravity", 10f);
+    }
+
+    private void ResetGravity()
+    {
+        gravityDirection = GravityDirection.DOWN;
+    }
+
+    private enum GravityDirection 
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
 }
